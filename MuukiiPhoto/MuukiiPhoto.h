0ef51d2bdaecf369672b11cf1c732998d32af0f6
muukii <muukii.muukii@gmail.com>
2014-09-22 03:07:03 +0900

    Initial Commit

---
+++ MuukiiPhoto/MuukiiPhoto.h
@@ -0,0 +1,19 @@
+//
+//  MuukiiPhoto.h
+//  MuukiiPhoto
+//
+//  Created by Muukii on 9/22/14.
+//  Copyright (c) 2014 Muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MuukiiPhoto.
+FOUNDATION_EXPORT double MuukiiPhotoVersionNumber;
+
+//! Project version string for MuukiiPhoto.
+FOUNDATION_EXPORT const unsigned char MuukiiPhotoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MuukiiPhoto/PublicHeader.h>
+
+
