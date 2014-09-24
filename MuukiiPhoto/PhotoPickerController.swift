7c0bcc7f51f785925601f6728e31622eba26cf9e
yusa <yusakzk@gmail.com>
2014-09-24 19:14:26 +0900

    addAsset追加

--- MuukiiPhoto/PhotoPickerController.swift
+++ MuukiiPhoto/PhotoPickerController.swift
@@ -10,8 +10,9 @@ import UIKit
 import Photos
 public class PhotoPickerController: UINavigationController {
 
-    let selectedItems: NSMutableArray?
-
+    let selectedItems: NSMutableArray? = NSMutableArray()
+    var maxNumSelectable : Int = 5
+    
     public class func photoPicker() -> PhotoPickerController {
         let storyboard = UIStoryboard(name: "MuukiiPhoto", bundle: NSBundle(identifier: "Muukii.MuukiiPhoto"))
         let controller = storyboard.instantiateViewControllerWithIdentifier(NSStringFromClass(PhotoPickerController)) as PhotoPickerController
@@ -23,4 +24,11 @@ public class PhotoPickerController: UINavigationController {
         // Do any additional setup after loading the view.
     }
 
+    func addAsset(asset:PHAsset){
+        if maxNumSelectable >= self.selectedItems?.count {
+            self.selectedItems?.addObject(asset)
+            println(self.selectedItems?.count)
+            println(asset)
+        }
+    }
 }
